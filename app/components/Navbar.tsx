d74e9983718e0dbc41445d11a7352cc55ac2c7dc
Amjad-722 <aliamjad722ee@gmail.com>
2024-07-29 13:38:19 +0500

    chnages in port name

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -52,7 +52,7 @@ const Navbar = () => {
 						/>
 					</Link>
 					<p className="text-white text-[18px] font-bold cursor-pointer flex">
-						Om &nbsp; <span className="sm:block hidden ">| omunite215</span>
+						A&nbsp; <span className="sm:block hidden ">| Amjad</span>
 					</p>
 				</div>
 
